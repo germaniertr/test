c47329c1eef1541ab1b37fadb47b5fe1d7411758
Dominique Huguenin <dominique.huguenin@rpn.ch>
2023-11-07 14:32:05 +0100

    feat: initialisation du projet

---
+++ src/main/java/recette/lib/RecetteLib.java
@@ -0,0 +1,15 @@
+package recette.lib;
+
+/**
+ *
+ * @author dominique huguenin <dominique.huguenin at rpn.ch>
+ */
+public final class RecetteLib {
+
+    private RecetteLib() {
+    }
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
