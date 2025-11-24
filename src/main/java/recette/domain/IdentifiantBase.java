590efe25f36655dc2934403941f49682906be46e
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-11-24 17:17:16 +0100

    feat: création de la classe IdentifiantBase vide

---
+++ src/main/java/recette/domain/IdentifiantBase.java
@@ -0,0 +1,14 @@
+package recette.domain;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class IdentifiantBase implements Identifiant {
+
+    @Override
+    public String getUUID() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
