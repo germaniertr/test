7c819cff404653301b155a3c7f63a6d3d7e3d4bd
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-11-24 17:18:38 +0100

    feat: création de la classe test vide  pour IdentifiantBase

---
+++ src/test/java/recette/domain/IdentifiantBaseTest.java
@@ -0,0 +1,29 @@
+package recette.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class IdentifiantBaseTest {
+
+    public IdentifiantBaseTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetUUID() {
+    }
+
+}
