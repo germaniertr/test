531235845ee34fa7ebac8f66a5a4ebf9adef3e3b
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-11-24 17:00:02 +0100

    feat: ajout de l'interface Ingrédient sans relation vers la recette

---
+++ src/main/java/recette/domain/Ingredient.java
@@ -0,0 +1,21 @@
+package recette.domain;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public interface Ingredient {
+
+    String getUUID();
+
+    void update(Ingredient ingredient);
+
+    String getNom();
+
+    void setNom(String nom);
+
+    String getDetail();
+
+    void setDetail(String detail);
+
+}
