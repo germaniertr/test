abdfcb8fba21e58af8d2d05860337de835fe79ef
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-11-25 10:54:29 +0100

    feat: ajout de la classe ComposantBase vide

---
+++ src/main/java/recette/domain/ComposantBase.java
@@ -0,0 +1,54 @@
+package recette.domain;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class ComposantBase implements Composant {
+
+    @Override
+    public Identifiant getIdentifiant() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(final Composant recette) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Double getQuantite() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setQuantite(final Double quantite) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getCommentaire() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setCommentaire(final String commentaire) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Ingredient getIngredient() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Unite getUnite() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setUnite(final Unite unite) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/test/java/recette/domain/ComposantBaseTest.java
@@ -0,0 +1,60 @@
+package recette.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class ComposantBaseTest {
+
+    public ComposantBaseTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetIdentifiant() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void testGetQuantite() {
+    }
+
+    @Test
+    public void testSetQuantite() {
+    }
+
+    @Test
+    public void testGetCommentaire() {
+    }
+
+    @Test
+    public void testSetCommentaire() {
+    }
+
+    @Test
+    public void testGetIngredient() {
+    }
+
+    @Test
+    public void testGetUnite() {
+    }
+
+    @Test
+    public void testSetUnite() {
+    }
+
+}
