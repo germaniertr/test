f79618cd8a3d1d59b0542211aed7c1b97630055a
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-11-25 16:39:46 +0100

    fix: implementation RecetteBase -  test les composants

--- src/test/java/recette/domain/RecetteBaseTest.java
+++ src/test/java/recette/domain/RecetteBaseTest.java
@@ -26,7 +26,7 @@ public class RecetteBaseTest {
     private Composant composantRef1;
     private Composant composantRef2;
     private Composant composantRef3;
-    private List<Object> composantsRef;
+    private List<Composant> composantsRef;
 
     public RecetteBaseTest() {
     }
@@ -91,6 +91,19 @@ public class RecetteBaseTest {
         Assertions.assertEquals(detailRef, this.entiteRef.getDetail());
         Assertions.assertEquals(preparationRef, this.entiteRef.getPreparation());
         Assertions.assertEquals(nombrePersonneRef, this.entiteRef.getNombrePersonnes());
+
+        Assertions.assertEquals(composantsRef.size(), this.entiteRef.getComposants().size());
+        for (int i = 0; i < composantsRef.size(); i += 1) {
+            Assertions.assertEquals(composantsRef.get(i).getIngredient(),
+                    this.entiteRef.getComposants().get(i).getIngredient());
+            Assertions.assertEquals(composantsRef.get(i).getCommentaire(),
+                    this.entiteRef.getComposants().get(i).getCommentaire());
+            Assertions.assertEquals(composantsRef.get(i).getQuantite(),
+                    this.entiteRef.getComposants().get(i).getQuantite());
+            Assertions.assertEquals(composantsRef.get(i).getUnite(),
+                    this.entiteRef.getComposants().get(i).getUnite());
+        }
+
     }
 
     @Test
@@ -100,10 +113,35 @@ public class RecetteBaseTest {
         String preparation = "nouvelle preparation";
         Integer nombrePersonne = 40;
 
+        Composant composant2 = new ComposantBase(composantRef2.getIdentifiant(),
+                composantRef2.getIngredient());
+        composant2.setCommentaire(composantRef2.getCommentaire());
+        composant2.setQuantite(composantRef2.getQuantite());
+        composant2.setUnite(composantRef2.getUnite());
+
+        composant2.setCommentaire("Composant modifié");
+        composant2.setQuantite(34.6);
+        composant2.setUnite(new UniteBase(new IdentifiantBase(UUID.randomUUID().toString())));
+
+        Composant composant4 = new ComposantBase(new IdentifiantBase(UUID.randomUUID().toString()),
+                new IngredientBase(new IdentifiantBase(UUID.randomUUID().toString())));
+        composant4.setCommentaire("commentaire 4");
+        composant4.setQuantite(45.7);
+        composant4.setUnite(new UniteBase(new IdentifiantBase(UUID.randomUUID().toString())));;
+
+        List<Composant> composants = new ArrayList<>();
+        composants.add(composant2);
+        composants.add(composant4);
+
         this.entiteRef.setNom(nom);
         this.entiteRef.setDetail(detail);
         this.entiteRef.setPreparation(preparation);
         this.entiteRef.setNombrePersonnes(nombrePersonne);
+
+        this.entiteRef.getComposants().get(1).update(composant2);
+        this.entiteRef.getComposants().remove(0);
+        this.entiteRef.getComposants().remove(1);
+        this.entiteRef.getComposants().add(composant4);
 
         Assertions.assertEquals(identifiantRef, this.entiteRef.getIdentifiant());
         Assertions.assertEquals(nom, this.entiteRef.getNom());
@@ -111,6 +149,16 @@ public class RecetteBaseTest {
         Assertions.assertEquals(preparation, this.entiteRef.getPreparation());
         Assertions.assertEquals(nombrePersonne, this.entiteRef.getNombrePersonnes());
 
+        for (int i = 0; i < composants.size(); i += 1) {
+            Assertions.assertEquals(composants.get(i).getIngredient(),
+                    this.entiteRef.getComposants().get(i).getIngredient());
+            Assertions.assertEquals(composants.get(i).getCommentaire(),
+                    this.entiteRef.getComposants().get(i).getCommentaire());
+            Assertions.assertEquals(composants.get(i).getQuantite(),
+                    this.entiteRef.getComposants().get(i).getQuantite());
+            Assertions.assertEquals(composants.get(i).getUnite(),
+                    this.entiteRef.getComposants().get(i).getUnite());
+        }
     }
 
     @Test
@@ -152,11 +200,35 @@ public class RecetteBaseTest {
         String preparation = "nouvelle preparation";
         Integer nombrePersonne = 40;
 
+        Composant composant2 = new ComposantBase(composantRef2.getIdentifiant(),
+                composantRef2.getIngredient());
+        composant2.setCommentaire(composantRef2.getCommentaire());
+        composant2.setQuantite(composantRef2.getQuantite());
+        composant2.setUnite(composantRef2.getUnite());
+
+        composant2.setCommentaire("Composant modifié");
+        composant2.setQuantite(34.6);
+        composant2.setUnite(new UniteBase(new IdentifiantBase(UUID.randomUUID().toString())));
+
+        Composant composant4 = new ComposantBase(new IdentifiantBase(UUID.randomUUID().toString()),
+                new IngredientBase(new IdentifiantBase(UUID.randomUUID().toString())));
+        composant4.setCommentaire("commentaire 4");
+        composant4.setQuantite(45.7);
+        composant4.setUnite(new UniteBase(new IdentifiantBase(UUID.randomUUID().toString())));;
+
+        List<Composant> composants = new ArrayList<>();
+        composants.add(composant2);
+        composants.add(composant4);
+
         Recette entite = new RecetteBase(identifiantRef);
         entite.setNom(nom);
         entite.setDetail(detail);
         entite.setPreparation(preparation);
         entite.setNombrePersonnes(nombrePersonne);
+
+        entite.getComposants().add(composant2);
+        entite.getComposants().add(composant4);
+
         this.entiteRef.update(entite);
 
         Assertions.assertEquals(identifiantRef, this.entiteRef.getIdentifiant());
@@ -164,6 +236,22 @@ public class RecetteBaseTest {
         Assertions.assertEquals(detail, this.entiteRef.getDetail());
         Assertions.assertEquals(preparation, this.entiteRef.getPreparation());
         Assertions.assertEquals(nombrePersonne, this.entiteRef.getNombrePersonnes());
+
+        Assertions.assertEquals(composants.size(), this.entiteRef.getComposants().size());
+        for (int i = 0; i < composants.size(); i += 1) {
+            Assertions.assertNotSame(composants.get(i),
+                    this.entiteRef.getComposants().get(i));
+            
+            Assertions.assertEquals(composants.get(i).getIngredient(),
+                    this.entiteRef.getComposants().get(i).getIngredient());
+            Assertions.assertEquals(composants.get(i).getCommentaire(),
+                    this.entiteRef.getComposants().get(i).getCommentaire());
+            Assertions.assertEquals(composants.get(i).getQuantite(),
+                    this.entiteRef.getComposants().get(i).getQuantite());
+            Assertions.assertEquals(composants.get(i).getUnite(),
+                    this.entiteRef.getComposants().get(i).getUnite());
+        }
+
     }
 
     @Test
@@ -175,6 +263,19 @@ public class RecetteBaseTest {
         Assertions.assertEquals(detailRef, this.entiteRef.getDetail());
         Assertions.assertEquals(preparationRef, this.entiteRef.getPreparation());
         Assertions.assertEquals(nombrePersonneRef, this.entiteRef.getNombrePersonnes());
+
+        Assertions.assertEquals(composantsRef.size(), this.entiteRef.getComposants().size());
+        for (int i = 0; i < composantsRef.size(); i += 1) {
+            Assertions.assertEquals(composantsRef.get(i).getIngredient(),
+                    this.entiteRef.getComposants().get(i).getIngredient());
+            Assertions.assertEquals(composantsRef.get(i).getCommentaire(),
+                    this.entiteRef.getComposants().get(i).getCommentaire());
+            Assertions.assertEquals(composantsRef.get(i).getQuantite(),
+                    this.entiteRef.getComposants().get(i).getQuantite());
+            Assertions.assertEquals(composantsRef.get(i).getUnite(),
+                    this.entiteRef.getComposants().get(i).getUnite());
+        }
+
     }
 
 }
