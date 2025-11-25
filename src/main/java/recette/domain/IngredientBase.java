15580da9682b890e62354356edebf44f8ba5d4ef
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-11-25 10:44:49 +0100

    feat: ajout de la classe IngredientBase vide

---
+++ src/main/java/recette/domain/IngredientBase.java
@@ -0,0 +1,49 @@
+package recette.domain;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class IngredientBase implements Ingredient {
+
+    @Override
+    public Identifiant getIdentifiant() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(final Ingredient ingredient) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getNom() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setNom(final String nom) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getDetail() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setDetail(final String detail) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Recette getRecette() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setRecette(final Recette recette) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/test/java/recette/domain/IngredientBaseTest.java
@@ -0,0 +1,56 @@
+package recette.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class IngredientBaseTest {
+
+    public IngredientBaseTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetIdentifiant() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void testGetNom() {
+    }
+
+    @Test
+    public void testSetNom() {
+    }
+
+    @Test
+    public void testGetDetail() {
+    }
+
+    @Test
+    public void testSetDetail() {
+    }
+
+    @Test
+    public void testGetRecette() {
+    }
+
+    @Test
+    public void testSetRecette() {
+    }
+
+}
