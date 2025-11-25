7efbdb30f698e7f36283bd639e3e175a3f037585
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-11-25 10:36:35 +0100

    feat: IdentifiantBase - implémentation de equals et hashcode

--- src/main/java/recette/domain/IdentifiantBase.java
+++ src/main/java/recette/domain/IdentifiantBase.java
@@ -1,5 +1,7 @@
 package recette.domain;
 
+import java.util.Objects;
+
 /**
  *
  * @author dominique huguenin (dominique.huguenin AT rpn.ch)
@@ -21,4 +23,29 @@ public class IdentifiantBase implements Identifiant {
     public String toString() {
         return "IdentifiantBase{" + "uuid=" + uuid + '}';
     }
+
+    @Override
+    public int hashCode() {
+        int hash = HASH;
+        hash = HASH2 * hash + Objects.hashCode(this.uuid);
+        return hash;
+    }
+    private static final int HASH2 = 61;
+    private static final int HASH = 7;
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Identifiant)) {
+            return false;
+        }
+        final Identifiant other = (Identifiant) obj;
+        return Objects.equals(this.uuid, other.getUUID());
+    }
+
 }
