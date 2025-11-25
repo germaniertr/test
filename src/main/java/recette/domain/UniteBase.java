fe471f3136e6724e9f63a445db4ff2aaedb28a00
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-11-25 10:40:29 +0100

    feat: ajout de la classe UniteBase vide

---
+++ src/main/java/recette/domain/UniteBase.java
@@ -0,0 +1,29 @@
+package recette.domain;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class UniteBase implements Unite {
+
+    @Override
+    public Identifiant getIdentifiant() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(final Unite unite) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getCode() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setCode(final String code) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/test/java/recette/domain/UniteBaseTest.java
@@ -0,0 +1,28 @@
+package recette.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class UniteBaseTest {
+
+    public UniteBaseTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+}
