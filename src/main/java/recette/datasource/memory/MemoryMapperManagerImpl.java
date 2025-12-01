2d603f3d5c6af596a9c7a4f319de95ab63d27b5a
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-12-01 16:56:15 +0100

    feat: implémentation DatabaseSetupTest pour recette.datasource.memory

--- src/main/java/recette/datasource/memory/MemoryMapperManagerImpl.java
+++ src/main/java/recette/datasource/memory/MemoryMapperManagerImpl.java
@@ -5,12 +5,21 @@ import recette.datasource.IngredientMapper;
 import recette.datasource.MapperManager;
 import recette.datasource.RecetteMapper;
 import recette.datasource.UniteMapper;
+import recette.domain.DemoData;
 
 /**
  *
  * @author dominique huguenin (dominique.huguenin@rpn.ch)
  */
 public class MemoryMapperManagerImpl implements MapperManager{
+
+    private MemoryMapperManagerImpl(DemoData demoData) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public static MapperManager getInstance(DemoData demoData){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     @Override
     public UniteMapper getUniteMapper() {
---
+++ src/test/java/recette/datasource/memory/DatabaseSetupImplTest.java
@@ -0,0 +1,37 @@
+package recette.datasource.memory;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import recette.datasource.MapperManager;
+import recette.domain.DemoData;
+import recette.domain.Ingredient;
+import recette.domain.Recette;
+import recette.domain.Unite;
+
+/**
+ *
+ * @author dominique huguenin <dominique.huguenin at rpn.ch>
+ */
+public class DatabaseSetupImplTest {
+
+    private final MapperManager mapperManager;
+
+    public DatabaseSetupImplTest() {
+        this.mapperManager = MemoryMapperManagerImpl.getInstance();
+    }
+
+    @Test
+    public void testDropCreateTable() throws Exception {
+        mapperManager.getDatabaseSetup().dropTables();
+        mapperManager.getDatabaseSetup().createTables();
+    }
+
+    @Test
+    public void testDropCreateTablesInsertData() throws Exception {
+        mapperManager.getDatabaseSetup().dropTables();
+        mapperManager.getDatabaseSetup().createTables();
+        mapperManager.getDatabaseSetup().insertData();
+    }
+
+}
