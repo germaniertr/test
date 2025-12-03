ac15c812d7a815609cd32d5ddb079f19da3dd4e7
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-12-03 11:16:30 +0100

    refactor: recette.datasource.db - généralisation de la classe de test recette.datasource.DatabaseSetupImplTest

---
+++ src/test/java/recette/datasource/DatabaseSetupImplTest.java
@@ -0,0 +1,48 @@
+package recette.datasource;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import recette.domain.Ingredient;
+import recette.domain.Recette;
+import recette.domain.Unite;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin at rpn.ch)
+ */
+
+public abstract class DatabaseSetupImplTest {
+    
+    protected final MapperManager mapperManager;
+
+    public DatabaseSetupImplTest(MapperManager mapperManager) {
+        this.mapperManager = mapperManager;
+    }
+
+    @Test
+    public void testDropCreateTable() throws Exception {
+        mapperManager.getDatabaseSetup().dropTables();
+        mapperManager.getDatabaseSetup().createTables();
+        List<Unite> unites = mapperManager.getUniteMapper().retrieve(".*");
+        Assertions.assertTrue(unites.isEmpty());
+        List<Ingredient> ingredients = mapperManager.getIngredientMapper().retrieve(".*");
+        Assertions.assertTrue(ingredients.isEmpty());
+        List<Recette> recettes = mapperManager.getRecetteMapper().retrieve(".*");
+        Assertions.assertTrue(recettes.isEmpty());
+    }
+
+    @Test
+    public void testDropCreateTablesInsertData() throws Exception {
+        mapperManager.getDatabaseSetup().dropTables();
+        mapperManager.getDatabaseSetup().createTables();
+        mapperManager.getDatabaseSetup().insertData();
+        List<Unite> unites = mapperManager.getUniteMapper().retrieve(".*");
+        Assertions.assertFalse(unites.isEmpty());
+        List<Ingredient> ingredients = mapperManager.getIngredientMapper().retrieve(".*");
+        Assertions.assertFalse(ingredients.isEmpty());
+        List<Recette> recettes = mapperManager.getRecetteMapper().retrieve(".*");
+        Assertions.assertFalse(recettes.isEmpty());
+    }
+    
+}
--- src/test/java/recette/datasource/memory/DatabaseSetupImplTest.java
+++ src/test/java/recette/datasource/memory/DatabaseSetupImplTest.java
@@ -1,63 +1,13 @@
 package recette.datasource.memory;
 
-import java.util.List;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import recette.datasource.MapperManager;
-import recette.domain.DemoData;
-import recette.domain.Ingredient;
-import recette.domain.Recette;
-import recette.domain.Unite;
-
 /**
  *
- * @author dominique huguenin <dominique.huguenin at rpn.ch>
+ * @author dominique huguenin (dominique.huguenin at rpn.ch)
  */
-public class DatabaseSetupImplTest {
-
-    private final MapperManager mapperManager;
+public class DatabaseSetupImplTest extends recette.datasource.DatabaseSetupImplTest {
 
     public DatabaseSetupImplTest() {
-        this.mapperManager = MemoryMapperManagerImpl.getInstance();
-    }
-
-    @Test
-    public void testDropCreateTable() throws Exception {
-        mapperManager.getDatabaseSetup().dropTables();
-        mapperManager.getDatabaseSetup().createTables();
-
-        List<Unite> unites = mapperManager.getUniteMapper()
-                .retrieve(".*");
-        Assertions.assertTrue(unites.isEmpty());
-
-        List<Ingredient> ingredients = mapperManager.getIngredientMapper()
-                .retrieve(".*");
-        Assertions.assertTrue(ingredients.isEmpty());
-
-        List<Recette> recettes = mapperManager.getRecetteMapper()
-                .retrieve(".*");
-        Assertions.assertTrue(recettes.isEmpty());
-
-    }
-
-    @Test
-    public void testDropCreateTablesInsertData() throws Exception {
-        mapperManager.getDatabaseSetup().dropTables();
-        mapperManager.getDatabaseSetup().createTables();
-        mapperManager.getDatabaseSetup().insertData();
-
-        List<Unite> unites = mapperManager.getUniteMapper()
-                .retrieve(".*");
-        Assertions.assertFalse(unites.isEmpty());
-
-        List<Ingredient> ingredients = mapperManager.getIngredientMapper()
-                .retrieve(".*");
-        Assertions.assertFalse(ingredients.isEmpty());
-
-        List<Recette> recettes = mapperManager.getRecetteMapper()
-                .retrieve(".*");
-        Assertions.assertFalse(recettes.isEmpty());
-
+        super(MemoryMapperManagerImpl.getInstance());
     }
 
 }
