216fc28f9d3158072886f87028a30009403b317f
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-12-03 16:34:38 +0100

    feat: recette.datasource.db - ajout des codes d'erreurs en provenance de postgresql

---
+++ src/main/java/core/datasource/db/SQL_ERREUR_CODES.java
@@ -0,0 +1,18 @@
+package core.datasource.db;
+//CHECKSTYLE.OFF: TypeName
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public final class SQL_ERREUR_CODES {
+
+    public static final class POSTGRESQL {
+
+        public static final String CONSTRAINT_UNIQUE_VIOLATION = "23505";
+        public static final String CONSTRAINT_NOT_NULL_VIOLATION = "23502";
+        public static final String CONSTRAINT_FOREIGN_KEY_VIOLATION = "23503";
+
+    }
+}
+//CHECKSTYLE.ON: TypeName
