5d3aa5a1f202527885c4ba22a461966247d3013f
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-12-03 11:07:35 +0100

    feat: recette.datasource.db - création de la classe de test pour DatabaseSetupImpl vide

---
+++ src/test/java/recette/datasource/db/DatabaseSetupImplTest.java
@@ -0,0 +1,36 @@
+package recette.datasource.db;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin@rpn.ch)
+ */
+public class DatabaseSetupImplTest {
+    
+    public DatabaseSetupImplTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCreateTables() throws Exception {
+    }
+
+    @Test
+    public void testDropTables() throws Exception {
+    }
+
+    @Test
+    public void testInsertData() throws Exception {
+    }
+    
+}
