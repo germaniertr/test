4c9a2da9c38777c10e1c14c6006f80b23ec2ac18
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-12-09 16:17:17 +0100

    refactor:  généralisation recette.datasource.db.IngredientMapperImpl

--- src/main/java/recette/datasource/db/IngredientMapperImpl.java
+++ src/main/java/recette/datasource/db/IngredientMapperImpl.java
@@ -1,25 +1,14 @@
 package recette.datasource.db;
 
-import core.datasource.ContrainteNotNullPersistenceException;
-import core.datasource.ContrainteUniquePersistenceException;
-import core.datasource.EntiteInconnuePersistenceException;
 import core.datasource.EntiteTropAnciennePersistenceException;
-import core.datasource.EntiteUtiliseePersistenceException;
 import core.datasource.PersistenceException;
-import core.datasource.db.SQL_ERREUR_CODES;
 import core.domain.Audit;
-import core.domain.AuditBase;
 import core.domain.Identifiant;
 import core.domain.IdentifiantBase;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.sql.Types;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import recette.datasource.IngredientMapper;
 import recette.datasource.RecetteRef;
 import recette.domain.Ingredient;
@@ -31,25 +20,22 @@ import recette.domain.RecetteBase;
  * @author dominique huguenin (dominique.huguenin@rpn.ch)
  */
 //CHECKSTYLE.OFF: MagicNumber
-public class IngredientMapperImpl implements IngredientMapper {
+public class IngredientMapperImpl
+        extends EntiteMapperImpl<Ingredient>
+        implements IngredientMapper {
 
-    private final DbMapperManagerImpl mapperManager;
-    private static final Logger LOG = Logger.getLogger(UniteMapperImpl.class.getName());
-
-    public IngredientMapperImpl(final DbMapperManagerImpl mm) {
-        this.mapperManager = mm;
+    IngredientMapperImpl(final DbMapperManagerImpl mm) {
+        super(mm,
+                SQL.INGREDIENTS.SELECT_BY_UUID,
+                SQL.INGREDIENTS.SELECT_BY_FILTRE,
+                SQL.INGREDIENTS.DELETE_BY_UUID);
     }
 
     @Override
-    public Ingredient create(final Ingredient entite) throws PersistenceException {
-        if (entite == null) {
-            return null;
-        }
-        Ingredient nouvelEntite = null;
-        Identifiant id = IdentifiantBase.builder().build();
-
+    protected void createEntity(final Identifiant id, final Ingredient entite)
+            throws SQLException, PersistenceException {
         try (PreparedStatement ps
-                = this.mapperManager.prepareStatement(SQL.INGREDIENTS.INSERT)) {
+                = this.getMapperManager().prepareStatement(SQL.INGREDIENTS.INSERT)) {
 
             ps.setString(1,
                     id.getUUID());
@@ -79,104 +65,15 @@ public class IngredientMapperImpl implements IngredientMapper {
             }
 
             ps.executeUpdate();
-
-            nouvelEntite = this.retrieve(id);
-
-        } catch (SQLException ex) {
-            LOG.log(Level.SEVERE, null, ex);
-            if (ex.getSQLState()
-                    .equals(SQL_ERREUR_CODES.POSTGRESQL.CONSTRAINT_NOT_NULL_VIOLATION)) {
-                throw new ContrainteNotNullPersistenceException(ex);
-            }
-            if (ex.getSQLState()
-                    .equals(SQL_ERREUR_CODES.POSTGRESQL.CONSTRAINT_UNIQUE_VIOLATION)) {
-                throw new ContrainteUniquePersistenceException(ex);
-            }
-            if (ex.getSQLState()
-                    .equals(SQL_ERREUR_CODES.POSTGRESQL.CONSTRAINT_FOREIGN_KEY_VIOLATION)) {
-                throw new EntiteInconnuePersistenceException(ex);
-            }
-
-            throw new PersistenceException(ex);
         }
-
-        return nouvelEntite;
 
     }
 
     @Override
-    public Ingredient retrieve(final Identifiant id) throws PersistenceException {
-        if (id == null) {
-            return null;
-        }
-        Ingredient entite = null;
-
+    protected void updateEntity(final Ingredient entite)
+            throws SQLException, PersistenceException {
         try (PreparedStatement ps
-                = this.mapperManager.prepareStatement(SQL.INGREDIENTS.SELECT_BY_UUID)) {
-            ps.setString(1, id.getUUID());
-
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                entite = readEntite(rs);
-            }
-        } catch (SQLException ex) {
-            LOG.log(Level.SEVERE, null, ex);
-            throw new PersistenceException(ex);
-        }
-
-        return entite;
-    }
-
-    @Override
-    public List<Ingredient> retrieve(final String filtre) throws PersistenceException {
-        List<Ingredient> ingredients = new ArrayList<>();
-
-        if (filtre == null) {
-            return ingredients;
-        }
-
-        try (PreparedStatement ps
-                = this.mapperManager.prepareStatement(SQL.INGREDIENTS.SELECT_BY_FILTRE)) {
-            ps.setString(1, filtre);
-
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Ingredient unite = readEntite(rs);
-                if (unite != null) {
-                    ingredients.add(unite);
-                }
-            }
-        } catch (SQLException ex) {
-            LOG.log(Level.SEVERE, null, ex);
-            throw new PersistenceException(ex);
-        }
-
-        return ingredients;
-
-    }
-
-    @Override
-    public void update(final Ingredient entite) throws PersistenceException {
-        if (entite == null) {
-            return;
-        }
-
-        if (entite.getIdentifiant() == null) {
-            return;
-        }
-
-        Ingredient entiteTrouvee
-                = this.retrieve(entite.getIdentifiant());
-        if (entiteTrouvee == null) {
-            throw new EntiteInconnuePersistenceException(
-                    String.format("Erreur: l'entitée (%s) "
-                            + "n'est pas connue!",
-                            entite.toString()));
-        }
-
-        /* traitement*/
-        try (PreparedStatement ps
-                = this.mapperManager.prepareStatement(SQL.INGREDIENTS.UPDATE)) {
+                = this.getMapperManager().prepareStatement(SQL.INGREDIENTS.UPDATE)) {
             if (entite.getNom() != null) {
                 ps.setString(1,
                         entite.getNom());
@@ -212,73 +109,11 @@ public class IngredientMapperImpl implements IngredientMapper {
                 throw new EntiteTropAnciennePersistenceException(
                         entite.toString());
             }
-
-        } catch (SQLException ex) {
-            LOG.log(Level.SEVERE, null, ex);
-            if (ex.getSQLState()
-                    .equals(SQL_ERREUR_CODES.POSTGRESQL.CONSTRAINT_NOT_NULL_VIOLATION)) {
-                throw new ContrainteNotNullPersistenceException(ex);
-            }
-            if (ex.getSQLState()
-                    .equals(SQL_ERREUR_CODES.POSTGRESQL.CONSTRAINT_UNIQUE_VIOLATION)) {
-                throw new ContrainteUniquePersistenceException(ex);
-            }
-            if (ex.getSQLState()
-                    .equals(SQL_ERREUR_CODES.POSTGRESQL.CONSTRAINT_FOREIGN_KEY_VIOLATION)) {
-                throw new EntiteInconnuePersistenceException(ex);
-            }
-
-            throw new PersistenceException(ex);
         }
-
     }
 
     @Override
-    public void delete(final Ingredient entite) throws PersistenceException {
-        /*pré-condition*/
-        if (entite == null) {
-            return;
-        }
-
-        if (entite.getIdentifiant() == null) {
-            return;
-        }
-
-        Ingredient entiteTrouvee
-                = this.retrieve(entite.getIdentifiant());
-        if (entiteTrouvee == null) {
-            throw new EntiteInconnuePersistenceException(
-                    String.format("Erreur: l'entitée (%s) "
-                            + "n'est pas connue!",
-                            entite.toString()));
-        }
-
-        /* traitement*/
-        try (PreparedStatement ps
-                = this.mapperManager.prepareStatement(SQL.INGREDIENTS.DELETE_BY_UUID)) {
-            ps.setString(1, entite.getIdentifiant().getUUID());
-
-            ps.setLong(2,
-                    entite.getVersion());
-
-            int row = ps.executeUpdate();
-            if (row == 0) {
-                throw new EntiteTropAnciennePersistenceException(
-                        entite.toString());
-            }
-        } catch (SQLException ex) {
-            LOG.log(Level.SEVERE, null, ex);
-            if (ex.getSQLState().equals(
-                    SQL_ERREUR_CODES.POSTGRESQL.CONSTRAINT_FOREIGN_KEY_VIOLATION)) {
-                throw new EntiteUtiliseePersistenceException(ex);
-            }
-
-            throw new PersistenceException(ex);
-        }
-
-    }
-
-    private Ingredient readEntite(final ResultSet rs) throws SQLException {
+    protected Ingredient readEntite(final ResultSet rs) throws SQLException {
         Identifiant identifiant
                 = readIdentifiant(rs);
         Long version = rs.getLong(SQL.ENTITES.ATTRIBUTS.VERSION);
@@ -311,41 +146,6 @@ public class IngredientMapperImpl implements IngredientMapper {
 
         return entite;
 
-    }
-
-    protected Identifiant readIdentifiant(final ResultSet rs)
-            throws SQLException {
-        String uuid = rs.getString(SQL.ENTITES.ATTRIBUTS.UUID);
-
-        return IdentifiantBase.builder()
-                .uuid(uuid)
-                .build();
-    }
-
-    protected Audit readAudit(final ResultSet rs)
-            throws SQLException {
-        Timestamp rsDateCreation
-                = rs.getTimestamp(SQL.ENTITES.ATTRIBUTS.DATE_CREATION);
-        String rsUserCreation
-                = rs.getString(SQL.ENTITES.ATTRIBUTS.USER_CREATION);
-        Timestamp rsDateModification
-                = rs.getTimestamp(SQL.ENTITES.ATTRIBUTS.DATE_MODIFICATION);
-        String rsUserModification
-                = rs.getString(SQL.ENTITES.ATTRIBUTS.USER_MODIFICATION);
-
-        AuditBase.Builder builder = AuditBase.builder()
-                .userCreation(rsUserCreation)
-                .userModification(rsUserModification);
-
-        if (rsDateCreation != null) {
-            builder.dateCreation(rsDateCreation.toInstant());
-        }
-
-        if (rsDateModification != null) {
-            builder.dateModification(rsDateModification.toInstant());
-        }
-
-        return builder.build();
     }
 
 }
