97f50a9926f974c46310174be495aa65b51ef01f
Dominique Huguenin <dominique.huguenin@rpn.ch>
2025-12-10 15:24:40 +0100

    feat: sérialisation json - recette.serialisation.json.IngredientJson sans recette

---
+++ src/main/java/recette/serialisation/json/IngredientJson.java
@@ -0,0 +1,124 @@
+package recette.serialisation.json;
+
+import core.domain.Audit;
+import core.domain.Identifiant;
+import core.serialisation.json.AuditJson;
+import core.serialisation.json.IdentifiantJson;
+import jakarta.json.bind.annotation.JsonbCreator;
+import jakarta.json.bind.annotation.JsonbProperty;
+import jakarta.json.bind.annotation.JsonbTransient;
+import jakarta.json.bind.annotation.JsonbTypeAdapter;
+import recette.domain.Ingredient;
+import recette.domain.IngredientBase;
+import recette.domain.Recette;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class IngredientJson implements Ingredient {
+
+    private Ingredient entite;
+
+    public IngredientJson(final Ingredient entite) {
+        if (entite == null) {
+            throw new NullPointerException();
+        }
+
+        this.entite = entite;
+    }
+
+    @JsonbCreator
+    public IngredientJson(
+            final @JsonbProperty("identifiant")
+            @JsonbTypeAdapter(IdentifiantJson.Adapter.class) Identifiant identifiant,
+            final @JsonbProperty("version") Long version,
+            final @JsonbProperty("audit")
+            @JsonbTypeAdapter(AuditJson.Adapter.class) Audit audit) {
+
+        this.entite = IngredientBase.builder()
+                .identifiant(identifiant)
+                .version(version)
+                .audit(audit)
+                .build();
+    }
+
+    @Override
+    @JsonbProperty("nom")
+    public String getNom() {
+        return this.entite.getNom();
+    }
+
+    @Override
+    @JsonbProperty("nom")
+    public void setNom(final String nom) {
+        this.entite.setNom(nom);
+    }
+
+    @Override
+    @JsonbProperty("detail")
+    public String getDetail() {
+        return this.entite.getDetail();
+    }
+
+    @Override
+    @JsonbProperty("detail")
+    public void setDetail(final String detail) {
+        this.entite.setDetail(detail);
+    }
+
+    @Override
+    @JsonbTransient
+    //@JsonbProperty("recette")
+    public Recette getRecette() {
+        return this.entite.getRecette();
+    }
+
+    @Override
+    @JsonbTransient
+    //@JsonbProperty("recette")
+    public void setRecette(final Recette recette) {
+        this.entite.setRecette(recette);
+    }
+
+    @Override
+    @JsonbProperty("identifiant")
+    @JsonbTypeAdapter(IdentifiantJson.Adapter.class)
+    public Identifiant getIdentifiant() {
+        return this.entite.getIdentifiant();
+    }
+
+    @Override
+    @JsonbProperty("version")
+    public Long getVersion() {
+        return this.entite.getVersion();
+    }
+
+    @Override
+    @JsonbProperty("audit")
+    @JsonbTypeAdapter(AuditJson.Adapter.class)
+    public Audit getAudit() {
+        return this.entite.getAudit();
+    }
+
+    @Override
+    public void update(final Ingredient pEntite) {
+        this.entite.update(pEntite);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.entite.hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return this.entite.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "IngredientJson{" + "entite=" + entite + '}';
+    }
+
+}
---
+++ src/test/java/recette/serialisation/json/IngredientJsonTest.java
@@ -0,0 +1,118 @@
+package recette.serialisation.json;
+
+import core.domain.Audit;
+import core.domain.AuditBase;
+import core.domain.Identifiant;
+import core.domain.IdentifiantBase;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbConfig;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.Instant;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import recette.domain.Ingredient;
+import recette.domain.IngredientBase;
+import recette.domain.Recette;
+import recette.domain.RecetteBase;
+
+/**
+ *
+ * @author dominique huguenin (dominique.huguenin AT rpn.ch)
+ */
+public class IngredientJsonTest {
+
+    private Identifiant identifiantRef;
+    private long versionRef;
+    private Audit auditRef;
+    private String nomRef;
+    private String detailRef;
+    private Recette recetteRef;
+    private Ingredient entiteRef;
+    private File fichierEntite;
+
+    public IngredientJsonTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        identifiantRef = IdentifiantBase.builder().build();
+        versionRef = 123L;
+        auditRef = AuditBase.builder()
+                .dateCreation(Instant.now())
+                .userCreation("user creation")
+                .dateModification(Instant.now().plusSeconds(60))
+                .userModification("user modification")
+                .build();
+
+        nomRef = "nom de référence";
+        detailRef = "détail de référence";
+
+        recetteRef = RecetteBase.builder()
+                .identifiant(IdentifiantBase.builder().build())
+                .build();
+
+        entiteRef = IngredientBase.builder()
+                .identifiant(identifiantRef)
+                .version(versionRef)
+                .audit(auditRef)
+                .nom(nomRef)
+                .detail(detailRef)
+                .recette(recetteRef)
+                .build();
+
+        fichierEntite = new File("target/ingredient.json");
+
+    }
+
+    @Test
+    public void testSerialisationDeserialisation() throws IOException {
+        //Sérialisation
+        try (OutputStream os = new FileOutputStream(fichierEntite)) {
+            JsonbConfig config = new JsonbConfig()
+                    .withFormatting(true)
+                    .withStrictIJSON(true);
+            Jsonb jsonb = JsonbBuilder.create(config);
+            jsonb.toJson(new IngredientJson(entiteRef), os);
+        }
+
+        //dé-sérialisation
+        Ingredient entite;
+        try (InputStream is = new FileInputStream(fichierEntite)) {
+            JsonbConfig config = new JsonbConfig()
+                    .withFormatting(true)
+                    .withStrictIJSON(true);
+            Jsonb jsonb = JsonbBuilder.create(config);
+            entite = jsonb.fromJson(is,
+                    IngredientJson.class);
+        }
+
+        Assertions.assertNotSame(this.entiteRef, entite);
+        Assertions.assertEquals(this.entiteRef, entite);
+        Assertions.assertEquals(this.entiteRef.hashCode(), entite.hashCode());
+
+        Assertions.assertEquals(this.entiteRef.getIdentifiant(), entite.getIdentifiant());
+        Assertions.assertEquals(this.entiteRef.getVersion(), entite.getVersion());
+        testAudit(this.entiteRef.getAudit(), entite.getAudit());
+
+        Assertions.assertEquals(this.entiteRef.getNom(), entite.getNom());
+        Assertions.assertEquals(this.entiteRef.getDetail(), entite.getDetail());
+//        Assertions.assertEquals(this.entiteRef.getRecette(), entite.getRecette());
+
+    }
+
+    private void testAudit(Audit ref, Audit audit) {
+        Assertions.assertEquals(ref, audit);
+        Assertions.assertEquals(ref.getUserCreation(),
+                audit.getUserCreation());
+        Assertions.assertEquals(ref.getUserModification(),
+                audit.getUserModification());
+    }
+
+}
